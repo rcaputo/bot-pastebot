7f68ef1c4fb58794a91c8a36325f5ceccc9b818a
Rocco Caputo <rcaputo@cpan.org>
2006-10-01 22:10:41 +0000

    Modernize slightly.

--- lib/Bot/Pastebot/Data.pm
+++ lib/Bot/Pastebot/Data.pm
@@ -1,7 +1,7 @@
 # Data management.
 # $Id$
 
-package Util::Data;
+package Bot::Pastebot::Data;
 
 use warnings;
 use strict;
@@ -9,7 +9,7 @@ use strict;
 use Carp qw(croak);
 use POE;
 use Storable;
-use Bot::Pastebot::Conf;
+use Bot::Pastebot::Conf qw( get_names_by_type get_items_by_name );
 
 use base qw(Exporter);
 
@@ -19,6 +19,8 @@ our @EXPORT_OK = qw(
   set_ignore clear_ignore get_ignores is_ignored channels add_channel
   remove_channel clear_channels
 );
+
+# Paste data members.
 
 sub PASTE_TIME    () { 0 }
 sub PASTE_SUMMARY () { 1 }
@@ -32,7 +34,7 @@ my %paste_cache;
 my %ignores; # $ignores{$ircnet}{lc $channel} = [ mask, mask, ... ];
 my %channels;
 
-# return a list of all paste ids
+# Return a list of all paste IDs.
 
 sub list_paste_ids {
   return keys %paste_cache;
@@ -55,8 +57,8 @@ sub list_paste_ids {
   }
 }
 
+# Remove pastes that are too old (if applicable).
 
-# remove pastes that are too old (if applicable)
 sub check_paste_count {
   my @names = get_names_by_type('pastes');
   return unless @names;
@@ -164,7 +166,7 @@ sub delete_paste {
   }
 }
 
-# manage channel/IRC network based ignores of http requestors
+# Manage channel/IRC network based ignores of http requestors.
 
 sub _convert_mask {
   my $mask = shift;
@@ -285,16 +287,16 @@ if (@pastes) {
   my %conf = get_items_by_name($pastes[0]);
   if ($conf{'check'} && $conf{'expire'}) {
     POE::Session->create(
-			inline_states => {
-				_start => sub { $_[KERNEL]->delay( ticks => $conf{'check'} ); },
-				ticks => sub {
-					for (keys %paste_cache) {
-						next unless (time - $paste_cache{$_}->[PASTE_TIME]) > $conf{'expire'};
-						delete_paste_by_id($_);
-					}
-					$_[KERNEL]->delay( ticks => $conf{'check'} );
-				},
-			},
+      inline_states => {
+        _start => sub { $_[KERNEL]->delay( ticks => $conf{'check'} ); },
+        ticks => sub {
+          for (keys %paste_cache) {
+            next unless (time - $paste_cache{$_}->[PASTE_TIME]) > $conf{'expire'};
+            delete_paste_by_id($_);
+          }
+          $_[KERNEL]->delay( ticks => $conf{'check'} );
+        },
+      },
     );
   }
 }
