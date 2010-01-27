10acddb55013a2f3535e9199b39bb0e0323398d8
Chris Nehren <apeiron@cpan.org>
2010-01-26 23:12:46 -0500

    Make the pidfile optional.

--- lib/Bot/Pastebot/Administrivia.pm
+++ lib/Bot/Pastebot/Administrivia.pm
@@ -9,7 +9,7 @@ use Carp qw(croak);
 use Bot::Pastebot::Conf qw( get_names_by_type get_items_by_name );
 
 use base qw(Exporter);
-our @EXPORT_OK = qw(get_pastebot_pid);
+our @EXPORT_OK = qw(get_pid write_pidfile uses_pidfile);
 
 # Return this module's configuration.
 
@@ -18,14 +18,20 @@ use Bot::Pastebot::Conf qw(SCALAR REQUIRED);
 my %conf = (
   administrivia => {
     name      => SCALAR | REQUIRED,
-    pidfile   => SCALAR | REQUIRED,
+    pidfile   => SCALAR,
   },
 );
 
 sub get_conf { return %conf }
 
+# Do we have a pidfile configured?
+sub uses_pidfile {
+  my %conf = get_items_by_name('administrivia');
+  return scalar %conf;
+}
+
 # Examine the PID file to see if there's a session running already.
-sub get_pastebot_pid {
+sub get_pid {
   my %conf = get_items_by_name('administrivia');
   my $pidfile = $conf{pidfile};
   return unless -e $pidfile;
@@ -39,11 +45,12 @@ sub get_pastebot_pid {
 }
 
 # We don't seem to be running, so write our PID file.
-sub write_pid_file {
+sub write_pidfile {
   my %conf = get_items_by_name('administrivia');
   my $pidfile = $conf{pidfile};
   open my $fh, '>', $pidfile or die "open($pidfile): $!";
   print $fh $$;
   close $fh;
 }
+
 1;
