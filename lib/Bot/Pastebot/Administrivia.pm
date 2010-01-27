abaa8d0b981b046e3b0744777c0e9c6e66f2589c
Chris Nehren <apeiron@cpan.org>
2010-01-26 23:35:01 -0500

    Add documentation and refactor the Administrivia section to be more flexible.

--- lib/Bot/Pastebot/Administrivia.pm
+++ lib/Bot/Pastebot/Administrivia.pm
@@ -24,15 +24,22 @@ my %conf = (
 
 sub get_conf { return %conf }
 
+# return the name used in the config for this section.
+sub _get_name {
+  my $name;
+  eval { ($name) = get_names_by_type('administrivia') };
+  return $name unless $@;
+}
+
 # Do we have a pidfile configured?
 sub uses_pidfile {
-  my %conf = get_items_by_name('administrivia');
-  return scalar %conf;
+  _get_name();
 }
 
 # Examine the PID file to see if there's a session running already.
 sub get_pid {
-  my %conf = get_items_by_name('administrivia');
+  my $name = _get_name();
+  my %conf = get_items_by_name($name);
   my $pidfile = $conf{pidfile};
   return unless -e $pidfile;
   my $pid = do {
@@ -46,7 +53,8 @@ sub get_pid {
 
 # We don't seem to be running, so write our PID file.
 sub write_pidfile {
-  my %conf = get_items_by_name('administrivia');
+  my $name = _get_name();
+  my %conf = get_items_by_name($name);
   my $pidfile = $conf{pidfile};
   open my $fh, '>', $pidfile or die "open($pidfile): $!";
   print $fh $$;
