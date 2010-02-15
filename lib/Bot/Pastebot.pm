ec7487f50fce9e6403b520997753f180996cb92f
Rocco Caputo <rcaputo@cpan.org>
2010-02-15 02:06:16 -0500

    Prepare for the next release.

---
+++ lib/Bot/Pastebot.pm
@@ -0,0 +1,27 @@
+package Bot::Pastebot;
+
+use warnings;
+use strict;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bot::Pastebot - the original clipboard to chat gateway
+
+=head1 SYNOPSIS
+
+pastebot -f configfile
+
+=head1 DESCRIPTION
+
+The L<pastebot> command-line program contains full documentation,
+including the syntax and options for pastebot's configuration files.
+
+Pastebot's documentation may be read by running C<perldoc pastebot>.
+
+The Bot::Pastebot module is a placeholder for later refactoring.
+
+=cut
